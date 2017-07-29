974eae1542f31dcf723f51867b1d4cc2b902c881
Richard Iyama Oluwaseun <richardiyama@yahoo.com>
2017-07-29 06:55:11 +0100

    Update SearchController.js

--- JavaScript/SearchController.js
+++ JavaScript/SearchController.js
@@ -5,7 +5,8 @@
 app.controller('SearchController', ['$scope','$animate','Flickr', function(scope,animate,Flickr){
 		
 		/*begin head animation*/
-		var header = angular.element(document.querySelector('header')),
+		
+	                var header = angular.element(document.querySelector('header')),
 			container = angular.element(document.querySelector('#container'));
 		animate.removeClass(header, 'showHead');
 		animate.addClass(header, 'reduceHead');
